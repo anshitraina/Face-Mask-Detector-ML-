b86aade810ebc717fe90b05a4c1153582d6c56c2
anshitraina <98584179+anshitraina@users.noreply.github.com>
2023-08-04 03:11:24 +0530

    updated detect_mask_video.py

--- detect_mask_video.py
+++ detect_mask_video.py
@@ -18,7 +18,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet):
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
 		(104.0, 177.0, 123.0))
 
-	# pass the blob through the network and obtain the face detections
+	# pass the blob through the network and obtain the face detections....
 	faceNet.setInput(blob)
 	detections = faceNet.forward()
 	print(detections.shape)
